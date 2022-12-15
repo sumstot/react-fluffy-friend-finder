10d49d4e038168c15b270ece7bc027481f52ec2e
Soren Umstot <soren.umstot@gmail.com>
2022-12-15 16:41:02 +0900

    Initial commit for my front end clone of AirBnB. I will be using our rails project Fluffy Friends Finder as a base

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+  return (
+    <nav className='nav'>
+      <img src="https://res.cloudinary.com/dfuvnptrs/image/upload/v1668757116/fff_logo_1_xvmxrz.svg" alt="Fluffy friend Finder logo" />
+    </nav>
+  );
+}
+
+
